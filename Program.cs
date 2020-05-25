aa44ed08d2ff0f4b9bf625d2955bcce77399ff4f
Miljan <miljan.dujovicmd@gmail.com>
2020-05-26 00:49:16 +0200

    TicTacToe first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(60, 30);
+			ConsoleKeyInfo info = Console.ReadKey();
+			int index = 0;
+			while (Console.ReadKey().Key != ConsoleKey.Escape)
+			{
+				
+				if (info.Key == ConsoleKey.DownArrow)
+				{
+					Console.SetCursorPosition(0, index + 1);
+				}
+				else if (info.Key == ConsoleKey.UpArrow)
+				{
+					Console.SetCursorPosition(0, index -1);
+				}
+				else if (info.Key == ConsoleKey.RightArrow)
+				{
+					Console.SetCursorPosition(index +1, 0);
+				}
+				else if (info.Key == ConsoleKey.LeftArrow)
+				{
+					Console.SetCursorPosition(index -1, 0);
+				}
+				index= index+1;
+				
+				info = Console.ReadKey();
+			}
+		}
+	}
+}
