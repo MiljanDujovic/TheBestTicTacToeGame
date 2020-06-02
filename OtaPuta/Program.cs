cf978a3aa8be372332e07fc44affae17f957c374
Miljan <miljan.dujovicmd@gmail.com>
2020-06-02 20:33:34 +0200

    Matrix 3x3

--- OtaPuta/Program.cs
+++ OtaPuta/Program.cs
@@ -8,36 +8,55 @@ namespace OtaPuta
 {
 	class Program
 	{
-		static public int[] row = new int[5] { 0, 0, 0, 2, 0 };
+		static public int[,] row = new int[3,3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 		static public void Print()
 		{
 			Console.Clear();
-			for (int i = 0; i < row.Length; i++)
+			for (int i = 0; i < 3; i++)
 			{
-				if (row[i] == 0)
+				for (int j = 0; j < 3; j++)
 				{
-					Console.Write("*");
+					if (row[i,j] == 0)
+					{
+						Console.Write("*");
+					}
+					else if (row[i,j] == 1)
+					{
+						Console.Write("X");
+					}
+					else if (row[i,j] == 2)
+					{
+						Console.Write("O");
+					}
 				}
-				else if (row[i] == 1)
-				{
-					Console.Write("X");
-				}
-				else if (row[i] == 2)
-				{
-					Console.Write("O");
-				}
+				Console.WriteLine();
 			}
 		}
+		static public Random rnd = new Random();
 		static public void OnInput(ConsoleKeyInfo keyInfo)
 		{
 			int.TryParse(keyInfo.KeyChar.ToString(), out int inputNumb);
-			if (inputNumb <= 5 && inputNumb >= 1)
+			if (inputNumb <= 3 && inputNumb >= 1)
 			{
-				if (row[inputNumb-1] ==0)
+				if (row[0,inputNumb-1] ==0)
 				{
-					row[inputNumb - 1] = 1;
+					row[0,inputNumb - 1] = 1;
 				}
 				
+			}
+			else if (inputNumb>3 && inputNumb<=6)
+			{
+				if (row[1, inputNumb - 4] == 0)
+				{
+					row[1, inputNumb - 4] = 1;
+				}
+			}
+			else if (inputNumb > 6 && inputNumb <= 9)
+			{
+				if (row[2, inputNumb - 7] == 0)
+				{
+					row[2, inputNumb - 7] = 1;
+				}
 			}
 		}
 		static void Main(string[] args)
